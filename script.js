c9155b8cd4b3ed86d8071f8fadf3f71048c41aa4
Eduardo Nahum <edunahum@gmail.com>
2020-08-09 21:04:39 -0400

    Project created

---
+++ script.js
@@ -0,0 +1,48 @@
+const gifContainer = document.querySelector('.gif-container');
+const gif = document.querySelector('.gif-container #gif');
+const points = document.querySelector('.gif-container #points');
+const word = document.querySelector('.word');
+const aboutBtn = document.getElementById('about-btn');
+const about = document.getElementById('about');
+const projectsBtn = document.getElementById('projects-btn');
+const projects = document.getElementById('projects');
+
+// Show gif events
+gifContainer.addEventListener("mousedown", () => {
+  gif.style.display = 'flex';
+  points.style.display = 'none';
+  clearInterval(timer);
+});
+
+gifContainer.addEventListener("mouseup", () => {
+  gif.style.display = 'none';
+  points.style.display = 'flex';
+  timer = setInterval(random, 200);
+});
+
+// Show random words
+const wordsArr = ['soccer', 'basketball', 'hockey', 'volleyball', 'baseball', 'football'];
+let count = 0;
+
+const random = () => {
+  let randomWord = wordsArr[count];
+  word.innerHTML = randomWord;
+  gif.src = './gif/' + randomWord + '.gif';
+  count ++;
+  
+  if (count === wordsArr.length) {
+    clearInterval(timer);
+    count = 0;
+
+    setTimeout(function() {
+      timer = setInterval(random, 200);
+    }, 20);
+  }
+};
+
+let timer = setInterval(random, 200);
+
+aboutBtn.addEventListener('mouseover', () => about.classList.add('show'));
+aboutBtn.addEventListener('mouseout', () => about.classList.remove('show'));
+projectsBtn.addEventListener('mouseover', () => projects.classList.add('show'));
+projectsBtn.addEventListener('mouseout', () => projects.classList.remove('show'));
